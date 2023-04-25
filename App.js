4411be7522b8363e5f6cfe47ec95509cf259a5b6
eugreg <daiaegregory@gmail.com>
2023-04-25 15:08:33 -0300

    adiçao de um tab

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { ScrollView } from 'react-native-web';
+import { Provider as PaperProvider } from 'react-native-paper';
 import Header from "./src/components/Header/Header"
+import Nav from "./src/components/Nav/Nav"
 export default function App() {
   return (
+    <PaperProvider>
+
     <View style={styles.body}>
       <Header />
       <ScrollView></ScrollView>
+      <Nav />
     </View>
+    </PaperProvider>
   );
 }
 
