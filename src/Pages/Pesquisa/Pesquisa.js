3eba45b1197b61ef9b0337ea3d21bcb20a20434f
eugreg <daiaegregory@gmail.com>
2023-05-02 14:59:36 -0300

    criaçao da pesquisa

--- src/Pages/Pesquisa/Pesquisa.js
+++ src/Pages/Pesquisa/Pesquisa.js
@@ -1,14 +1,31 @@
-import { Text, StyleSheet, View } from 'react-native'
-import React, { Component } from 'react'
+import { ScrollView, View } from 'react-native';
+import { TextInput } from 'react-native-paper';
+import styles from "././styles";
+import Card from  "../../components/Ofertas/CardProduto/Card"
+const Pesquisa = () => {
 
-export default class Pesquisa extends Component {
-  render() {
-    return (
-      <View>
-        <Text>Pesquisa</Text>
-      </View>
-    )
-  }
-}
 
-const styles = StyleSheet.create({})
+  return (
+    <ScrollView>
+
+    <TextInput
+      
+      mode="outlined"
+      label="pesquisar"
+      placeholder="pesquisar"
+      right={<TextInput.Affix  />}
+      
+      />
+      <View style={styles.produtos}>
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+        </View>
+    </ScrollView>
+  );
+};
+
+export default Pesquisa
